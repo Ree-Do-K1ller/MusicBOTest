6e4de6e9180e25789e2fb650c88c54a8a09b8212
Ree-Do-K1ller <162260484+Ree-Do-K1ller@users.noreply.github.com>
2024-03-05 17:38:40 -0300

    Update filter.js

--- commands/filter.js
+++ commands/filter.js
@@ -9,7 +9,7 @@ module.exports = {
     try {
       const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
       const queue = client?.player?.getQueue(interaction?.guild?.id);
-      if (!queue || !queue?.playing) return interaction?.reply({ content: '⚠️ No music playing!!', ephemeral: true }).catch(e => { })
+      if (!queue || !queue?.playing) return interaction?.reply({ content: '**( ⚠️ )** Nenhuma música está tocando no momento.', ephemeral: true }).catch(e => { })
 
       let buttons = new ActionRowBuilder().addComponents(
         new ButtonBuilder()
@@ -51,13 +51,13 @@ module.exports = {
       
 
       let embed = new EmbedBuilder()
-      .setColor('#01fe66')
+      .setColor('#922cc96')
       .setAuthor({
           name: 'Audio Filters ',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157534645311766558/2353-arrowrightglow.gif?ex=6518f5a5&is=6517a425&hm=ce55696f7ed85e2f7a97a3505eb39016fa9cd0c50be043efdf0cce06d7126b4c&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-      .setDescription('** Explore the Beat, Choose Your Sound Magic Below!**')
+      .setDescription('** Explore a batida, escolha sua magia sonora abaixo: <:heheh:1025172645244850197>**')
   
     interaction.reply({ embeds: [embed], components: [buttons, buttons2] }).then(async Message => {
 
@@ -71,22 +71,22 @@ module.exports = {
 if(!filters?.includes(button?.customId)) return
 
       let filtre = button.customId
-      if (!filtre) return interaction?.editReply({ content: '❌ Invalid Name', ephemeral: true }).catch(e => { })
+      if (!filtre) return interaction?.editReply({ content: '**( ❌ )** Nome inválido.', ephemeral: true }).catch(e => { })
      filtre = filtre?.toLowerCase()
    
       if (filters?.includes(filtre?.toLowerCase())) {
         if (queue?.filters?.has(filtre)) {
           queue?.filters?.remove(filtre)
-          embed?.setDescription(`Magic : **{filter}**, Applied Status: **{status}**`.replace("{filter}", filtre).replace("{status}", "❌"))
+          embed?.setDescription(`Mágica : **{filter}**, Status Aplicado: **{status}**`.replace("{filter}", filtre).replace("{status}", "❌"))
           return interaction?.editReply({ embeds: [embed] }).catch(e => { })
         } else {
           queue?.filters?.add(filtre)
-          embed?.setDescription(`Magic : **{filter}**, Applied Status: **{status}**`.replace("{filter}", filtre).replace("{status}", "✅"))
+          embed?.setDescription(`Mágica : **{filter}**, Applied Status: **{status}**`.replace("{filter}", filtre).replace("{status}", "✅"))
           return interaction?.editReply({ embeds: [embed] }).catch(e => { })
         }
       } else {
         const filter = filters?.find((x) => x?.toLowerCase() === filtre?.toLowerCase())
-        embed?.setDescription(`❌ Couldn't find filter!!`.replace("{filters}", filters?.map(mr => `\`${mr}\``).join(", ")))
+        embed?.setDescription(`**( ❌ )** Não foi possível encontrar o filtro.`.replace("{filters}", filters?.map(mr => `\`${mr}\``).join(", ")))
         if (!filter) return interaction?.editReply({ embeds: [embed] }).catch(e => { })
       }
     })
