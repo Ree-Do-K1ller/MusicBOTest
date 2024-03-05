cd17c2656afa455f1c04418c6450cf9706d78ffa
Ree-Do-K1ller <162260484+Ree-Do-K1ller@users.noreply.github.com>
2024-03-05 18:39:04 -0300

    Update nowplaying.js

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -1,28 +1,28 @@
 const { EmbedBuilder } = require('discord.js');
 const db = require("../mongoDB");
 module.exports = {
-  name: "nowplaying",
-  description: "get info of current song.",
+  name: "tocando-agora",
+  description: "Obter informações da música atual.",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
     try {
 
       const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ No music playing!!`, ephemeral: true }).catch(e => { })
+      if (!queue || !queue.playing) return interaction.reply({ content: `**( ⚠️ )** Nenhuma música está tocando no momento.`, ephemeral: true }).catch(e => { })
 
       const track = queue.songs[0];
-      if (!track) return interaction.reply({ content: `⚠️ No music playing!!`, ephemeral: true }).catch(e => { })
+      if (!track) return interaction.reply({ content: `**( ⚠️ )** Nenhuma música está tocando no momento.`, ephemeral: true }).catch(e => { })
 
       const embed = new EmbedBuilder();
       embed.setColor(client.config.embedColor);
       embed.setThumbnail(track.thumbnail);
       embed.setTitle(track.name)
-      embed.setDescription(`> **Audio** \`%${queue.volume}\`
+      embed.setDescription(`> **Áudio** \`%${queue.volume}\`
 > **Duration :** \`${track.formattedDuration}\`
 > **URL :** **${track.url}**
-> **Loop Mode :** \`${queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'}\`
-> **Filter**: \`${queue.filters.names.join(', ') || 'Off'}\`
+> **Loop Mode :** \`${queue.repeatMode ? (queue.repeatMode === 2 ? 'Todas as Filas' : 'Esta música') : 'Desligada'}\`
+> **Filter**: \`${queue.filters.names.join(', ') || 'Desligada'}\`
 > **By :** <@${track.user.id}>`);
 
 
