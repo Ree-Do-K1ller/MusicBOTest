bb4e537fb51d18edc7121d3654cdd03a7e0e6310
Ree-Do-K1ller <162260484+Ree-Do-K1ller@users.noreply.github.com>
2024-03-05 17:40:50 -0300

    Update autoplay.js

--- commands/autoplay.js
+++ commands/autoplay.js
@@ -19,14 +19,14 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "autoplay",
-  description: "Toggle the autoplay of the queue.",
+  description: "Alterne a reprodução automática da fila.",
   options: [],
   permissions: "0x0000000000000800",
   run: async (client, interaction) => {
     try {
       const queue = client?.player?.getQueue(interaction?.guild?.id);
       if (!queue || !queue?.playing) {
-        return interaction?.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+        return interaction?.reply({ content: '**( ⚠️ )** Nenhuma música está tocando no momento.', ephemeral: true });
       }
       
       queue?.toggleAutoplay();
@@ -34,7 +34,7 @@ module.exports = {
       const embed = new EmbedBuilder()
         .setColor('#2f58fe')
         .setTitle('Your Music, Your Call!!')
-        .setDescription(queue?.autoplay ? '**✅ Autoplay ON**' : '**❌ Autoplay OFF**')
+        .setDescription(queue?.autoplay ? '**( ✅ )** Autoplay: **ON**' : '**( ❌ )** Autoplay: **OFF**')
         
       
       interaction?.reply({ embeds: [embed] });
