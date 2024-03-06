fa60bbe4a64ccecac2c673526e38150639072c19
Ree-Do-K1ller <162260484+Ree-Do-K1ller@users.noreply.github.com>
2024-03-05 22:05:46 -0300

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -39,7 +39,7 @@ module.exports = {
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157296313013117049/8061-purple-pause-icon.png?ex=651817ae&is=6516c62e&hm=4596c9fab9d8b66de8b5215b2750572ced352eed67440a1134550b846b5693b9&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-        .setDescription(success ? '**The music has been Paused for a moment!!**' : '**( ❌ )** Erro de comando: não foi possível pausar a música.')
+        .setDescription(success ? '**A música foi pausada por um momento.**' : '**( ❌ )** Erro de comando: não foi possível pausar a música.')
         
 
       return interaction.reply({ embeds: [embed] });
