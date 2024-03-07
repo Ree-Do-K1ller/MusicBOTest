e6538e63633f565021f334bf87212b7c9443d9bc
Ree-Do-K1ller <162260484+Ree-Do-K1ller@users.noreply.github.com>
2024-03-07 20:02:32 -0300

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -16,12 +16,12 @@
 const { ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
 const db = require("../mongoDB");
 module.exports = {
-  name: "skip",
-  description: "Switches the music being played.",
+  name: "pular",
+  description: "Pule a música que está sendo reproduzida.",
   permissions: "0x0000000000000800",
   options: [{
     name: "number",
-    description: "mention how many songs you wanna skip",
+    description: "mencione quantas músicas você deseja pular.",
     type: ApplicationCommandOptionType.Number,
     required: false
   }],
@@ -31,45 +31,44 @@ module.exports = {
     try {
 
       const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true }).catch(e => { })
+      if (!queue || !queue.playing) return interaction.reply({ content: '**( ⚠️ )** Nenhuma música está tocando no momento.', ephemeral: true }).catch(e => { })
 
       let number = interaction.options.getNumber('number');
       if (number) {
-        if (!queue.songs.length > number) return interaction.reply({ content: '⚠️ Exceeded current no of songs', ephemeral: true }).catch(e => { })
-        if (isNaN(number)) return interaction.reply({ content: '⚠️ Invalid Number', ephemeral: true }).catch(e => { })
-        if (1 > number) return interaction.reply({ content: '⚠️ Invalid Number', ephemeral: true }).catch(e => { })
+        if (!queue.songs.length > number) return interaction.reply({ content: '**( ⚠️ )** Excedeu o número atual de músicas.', ephemeral: true }).catch(e => { })
+        if (isNaN(number)) return interaction.reply({ content: '**( ⚠️ )** Número inválido.', ephemeral: true }).catch(e => { })
+        if (1 > number) return interaction.reply({ content: '**( ⚠️ )** Número inválido.', ephemeral: true }).catch(e => { })
 
         try {
         let old = queue.songs[0];
         await client.player.jump(interaction, number).then(song => {
-          return interaction.reply({ content: `⏯️ Skipped : **${old.name}**` }).catch(e => { })
+          return interaction.reply({ content: `**( ⏯️ )** Pulou: **${old.name}**` }).catch(e => { })
         })
       } catch(e){
-        return interaction.reply({ content: '❌ Queue is empty!!', ephemeral: true }).catch(e => { })
+        return interaction.reply({ content: '**( ❌ )** A fila está vazia.', ephemeral: true }).catch(e => { })
       }
       } else {
 try {
   const queue = client.player.getQueue(interaction.guild.id);
   if (!queue || !queue.playing) {
-    return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+    return interaction.reply({ content: '**( ⚠️ )** Nenhuma música está tocando no momento.', ephemeral: true });
   }
 
   let old = queue.songs[0];
   const success = await queue.skip();
 
   const embed = new EmbedBuilder()
-    .setColor('#3498db')
+    .setColor('#a334db')
     .setAuthor({
-      name: 'Song Skipped',
-      iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157269773118357604/giphy.gif?ex=6517fef6&is=6516ad76&hm=f106480f7d017a07f75d543cf545bbea01e9cf53ebd42020bd3b90a14004398e&',
-      url: 'https://discord.gg/FUEHs7RCqz'
+      name: 'Música ignorada',
+      iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157269773118357604/giphy.gif?ex=6517fef6&is=6516ad76&hm=f106480f7d017a07f75d543cf545bbea01e9cf53ebd42020bd3b90a14004398e&'
     })
-    .setDescription(success ? ` **SKIPPED** : **${old.name}**` : '❌ Queue is empty!')
+    .setDescription(success ? ` **PULADO:** _${old.name}_` : '**( ❌ )** A fila está vazia.')
     .setTimestamp();
 
   return interaction.reply({ embeds: [embed] });
 }catch (e) {
-          return interaction.reply({ content: '❌ Queue is empty!!', ephemeral: true }).catch(e => { })
+          return interaction.reply({ content: '**( ❌ )** A fila está vazia.', ephemeral: true }).catch(e => { })
         }
       }
 
