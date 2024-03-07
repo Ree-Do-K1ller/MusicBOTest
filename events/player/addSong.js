d3dfb41c66d9e37be9a9b439e24404b5de1c0b2e
Ree-Do-K1ller <162260484+Ree-Do-K1ller@users.noreply.github.com>
2024-03-06 22:13:51 -0300

    Update addSong.js

--- events/player/addSong.js
+++ events/player/addSong.js
@@ -8,11 +8,10 @@ module.exports = async (client, queue, song) => {
       const embed = new EmbedBuilder()
         .setAuthor({
         name: 'Adicionado à fila',
-        iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157218651179597884/1213-verified.gif?ex=6517cf5a&is=65167dda&hm=bc8fb4414cb412587ade0af285b77569d2568214cf7d6baab8702ddeb6c38ad5&', 
-        url: 'https://discord.gg/FUEHs7RCqz'
+        iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157218651179597884/1213-verified.gif?ex=6517cf5a&is=65167dda&hm=bc8fb4414cb412587ade0af285b77569d2568214cf7d6baab8702ddeb6c38ad5&'
     })
         .setDescription(`<@${song.user.id}>, **${song.name}**`)
-        .setColor('#14bdff')
+        .setColor('#00ff6e')
         .setFooter({ text: 'Use /fila para mais informações.' });
       queue?.textChannel?.send({ embeds: [embed] }).catch(e => { });
     }
