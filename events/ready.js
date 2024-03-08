2f9dd94781950dd82c11d53c7595974821eb3880
Ree-Do-K1ller <162260484+Ree-Do-K1ller@users.noreply.github.com>
2024-03-08 18:54:40 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -21,7 +21,7 @@ console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
 
 console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`)
 
-setInterval(() => client.user.setPresence({status: 'dnd', activity: {name: 'Sua bunda.', type: 'WATCHING'}}), 10000);
+setInterval(() => client.user.setPresence({ activities: [{ name: `discord.js v14`, type: `WATCHING` }], status: 'dnd' }), 10000);
 client.errorLog = config.errorLog
 } else {
 console.log('\x1b[36m%s\x1b[0m', `|    🍔 Error MongoDB!`)
