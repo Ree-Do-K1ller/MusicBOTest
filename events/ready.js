e0365747bb6c530f8d2d2b2ff7bb0083c59f9625
Ree-Do-K1ller <162260484+Ree-Do-K1ller@users.noreply.github.com>
2024-03-08 17:46:10 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -19,8 +19,17 @@ console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
 }
 })();
 
-console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
+console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`)
 
+setInterval(() => client.user.setPresence({status: 'dnd', activity: {name: 'Sua bunda.', type: 'WATCHING'}}), 10000);
+client.errorLog = config.errorLog
+} else {
+console.log('\x1b[36m%s\x1b[0m', `|    🍔 Error MongoDB!`)
+}
+console.log('\x1b[36m%s\x1b[0m', `|    🎯 Activity sucessfully set!`);
+
+/*
+##########################
 bot.user.setPresence({
    status: 'dnd',
    activity: {
@@ -28,6 +37,7 @@ bot.user.setPresence({
        type: 'WATCHING'
     }
  });
+##########################
 
 setInterval(() => client.user.setActivity({ 
   name: `Yuri's Reality.`, 
@@ -37,7 +47,8 @@ client.errorLog = config.errorLog
 console.log('\x1b[36m%s\x1b[0m', `|    🍔 Error MongoDB!`)
 }
 console.log('\x1b[36m%s\x1b[0m', `|    🎯 Activity sucessfully set!`);
-
+*/
+   
 if(client.config.voteManager.status === true && client.config.voteManager.api_key){
 const { AutoPoster } = require('topgg-autoposter')
 const ap = AutoPoster(client.config.voteManager.api_key, client)
